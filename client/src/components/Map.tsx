70914430a080c73664d027229a20ae3bc944ce76
jeffreycao1998 <jeffreycao1998@hotmail.com>
2021-01-16 09:28:22 -0500

    can draw a polyline on map

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { MAPBOX_ACCESS_TOKEN } from '../util/constants';
 import { Viewport } from '../types';
+import axios from 'axios';
 
 // Components
 import ReactMapGL, { Marker, Source, Layer } from 'react-map-gl';
@@ -22,18 +23,19 @@ const Map = () => {
 
   useEffect(() => {
     setViewport({
-      longitude: -79.340424,
-      latitude: 43.795712,
+      longitude: -122.48369693756104,//-79.340424,
+      latitude: 37.83381888486939,//43.795712,
       zoom: 8
     })
-  },[])
+  },[]);
 
-  const geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {type: 'Feature', geometry: {type: 'Point', coordinates: [-122.4, 37.8]}}
-    ]
-  };
+  axios.get("https://api.mapbox.com/optimized-trips/v1/mapbox/driving/-79.340424,43.795712;-75.340424,38.795712?access_token=pk.eyJ1IjoiamVmZnJleWNhbzE5OTgiLCJhIjoiY2tldDIzdWdqMG5xZDJwcGVjYmpiYnE0NiJ9._mpFf2LjB-ZFa1NC5xUU1Q")
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
   return (
     <Container>
@@ -56,6 +58,56 @@ const Map = () => {
             )
           })
         } */}
+        
+        <Source 
+          id="my-data" 
+          type="geojson" 
+          data={{
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: [
+                [-122.48369693756104, 37.83381888486939],
+                [-122.48348236083984, 37.83317489144141],
+                [-122.48339653015138, 37.83270036637107],
+                [-122.48356819152832, 37.832056363179625],
+                [-122.48404026031496, 37.83114119107971],
+                [-122.48404026031496, 37.83049717427869],
+                [-122.48348236083984, 37.829920943955045],
+                [-122.48356819152832, 37.82954808664175],
+                [-122.48507022857666, 37.82944639795659],
+                [-122.48610019683838, 37.82880236636284],
+                [-122.48695850372314, 37.82931081282506],
+                [-122.48700141906738, 37.83080223556934],
+                [-122.48751640319824, 37.83168351665737],
+                [-122.48803138732912, 37.832158048267786],
+                [-122.48888969421387, 37.83297152392784],
+                [-122.48987674713133, 37.83263257682617],
+                [-122.49043464660643, 37.832937629287755],
+                [-122.49125003814696, 37.832429207817725],
+                [-122.49163627624512, 37.832564787218985],
+                [-122.49223709106445, 37.83337825839438],
+                [-122.49378204345702, 37.83368330777276]
+              ]
+            }
+          }}
+        >
+          <Layer
+            id='route'
+            type='line'
+            source='route'
+            layout={{
+              'line-join': 'round',
+              'line-cap': 'round'
+            }}
+            paint={{
+              'line-color': '#029ffa',
+              'line-width': 8
+            }}
+          />
+        </Source>
+
       </ReactMapGL>
     </Container>
   )
