9beb52ca7044ea17f10ce85b15182743a1dcd59f
jeffreycao1998 <jeffreycao1998@hotmail.com>
2021-01-22 22:53:18 -0500

    doesn't add sequences with invalid start points and invalid pickup/dropoff orders for the brute force algorithm

--- server/src/util/calcOptimalRouteV2.ts
+++ server/src/util/calcOptimalRouteV2.ts
@@ -2,8 +2,6 @@
 ////////////// BRUTE FORCE ALGORITHM (does not scale very well at all)
 //////////////
 
-// import axios from 'axios';
-// import fs from 'fs';
 import { 
   shipments,
   startTime 
@@ -13,6 +11,8 @@ import {
   ShipmentType,
 } from '../types';
 import { initIndexArray, swap } from './helpers';
+
+const STARTING_POINT = 0;
 
 const extractWaypoints = (shipments: Array<ShipmentType>) => {
   const waypoints = [];
@@ -33,8 +33,33 @@ const extractWaypoints = (shipments: Array<ShipmentType>) => {
   return waypoints;
 };
 
+const checkValidSequence = (indexes: Array<number>) => {
+  const set = new Set();
+  set.add(STARTING_POINT);
+
+  for (let i = 1; i < indexes.length; i++) {
+    // is a pickup location so just add it to the set
+    if (indexes[i] % 2 === 0) {
+      set.add(indexes[i]);
+
+    // is a dropoff location so check if it was picked up first
+    } else if (indexes[i] % 2 === 1 && !set.has(indexes[i] - 1)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+let totalCombinations = 0;
+
 export const getAllCombinations = (indexes: Array<number>, currentIndex: number, combinations = [] as Array<Array<number>>) => {
   if (currentIndex === indexes.length - 1) {
+    if (indexes[0] !== STARTING_POINT) return;
+    if (!checkValidSequence(indexes)) return;
+
+    totalCombinations += 1;
+
     combinations.push([...indexes]);
   }
 
@@ -57,16 +82,13 @@ const getAllTotalDistances = async (shipments: Array<ShipmentType>) => {
   let shortestDistanceCombination = null;
   let shortestDistance = Infinity;
 
+  // @ts-ignore
   for (let combination of combinations) {
     let lastIndex = null;
     let totalDistance = 0;
     combination.push(combination[0]);
 
     // skip combination if combination doesn't start where we want it to start
-    const startingPoint = 0;
-    if (combination[0] !== startingPoint) {
-      continue;
-    }
 
     for (let index of combination) {
       if (lastIndex === null) {
@@ -89,6 +111,7 @@ const getAllTotalDistances = async (shipments: Array<ShipmentType>) => {
   const endTime = (new Date).getTime();
 
   console.log({
+    totalCombinations,
     shortestDistanceCombination,
     shortestDistance,
     algorithmDurationMs: endTime - startTime
